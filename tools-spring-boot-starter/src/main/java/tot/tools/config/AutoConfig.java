78148f9cd99f6c9dc729967496655ea90b8df4a3
lingduliudu <lingduliudu@163.com>
2020-04-10 14:11:29 +0800

    tools 工具

---
+++ tools-spring-boot-starter/src/main/java/tot/tools/config/AutoConfig.java
@@ -0,0 +1,16 @@
+package tot.tools.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import tot.tools.service.ConditionalClass;
+
+@Configuration
+@ComponentScan(basePackages = "tot")
+@ConditionalOnClass(ConditionalClass.class)
+public class AutoConfig {
+	
+	
+
+}
---
+++ tools-spring-boot-starter/src/main/java/tot/tools/config/CompnentTest.java
@@ -0,0 +1,9 @@
+package tot.tools.config;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class CompnentTest {
+
+}
