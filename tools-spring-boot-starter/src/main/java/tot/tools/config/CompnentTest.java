0a63a7c89687b014635cdd0aa65f48484cb99150
lingduliudu <lingduliudu@163.com>
2020-04-10 14:15:37 +0800

    添加功能

--- tools-spring-boot-starter/src/main/java/tot/tools/config/CompnentTest.java
+++ tools-spring-boot-starter/src/main/java/tot/tools/config/CompnentTest.java
@@ -1,9 +1,15 @@
 package tot.tools.config;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class CompnentTest {
 
+	@PostConstruct
+	public void init() {
+		System.out.println("init tools success... ");
+	}
 }
